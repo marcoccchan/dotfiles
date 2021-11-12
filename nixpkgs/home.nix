aea6ede1fd0699d0981fb097d5139868b90efb27
Marco Chan <marco@Marcos-MacBook-Pro.local>
2021-11-12 10:18:14 +0000

    Update home location

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,23 +1,38 @@
 { config, pkgs, ... }:
 
+let
+  HOME = config.home.homeDirectory;
+in
 {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "marco.chan";
-  home.homeDirectory = "/Users/marco.chan";
+  home.username = "marco";
+  home.homeDirectory = "/Users/marco";
 
   # Packages to install
   home.packages = [
     # pkgs is the set of all packages in the default home.nix implementation
     pkgs.tmux
+    pkgs.zsh
+    pkgs.clojure
+    pkgs.openjdk11
   ];
+
+  programs.zsh = {
+    enable = true;
+    profileExtra = ''
+      . ${HOME}/.nix-profile/etc/profile.d/nix.sh
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    '';
+  };
 
   # Raw configuration files
   home.file.".tmux.conf".source = ./tmux.conf;
 
+  
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
